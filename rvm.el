af1e26023d8dd5c08b53e5f36dc90484e864265f
Roman Scherer <roman@soundcloud.com>
2010-03-26 09:49:40 +0100

    Added rvm.el.

---
+++ rvm.el
@@ -0,0 +1,8 @@
+
+(setenv "GEM_HOME" "/usr/local/rvm/gems/ruby-1.9.1-p378")
+(setenv "GEM_PATH" "/usr/local/rvm/gems/ruby-1.9.1-p378:/usr/local/rvm/gems/ruby-1.9.1-p378%global")
+(setenv "BUNDLE_PATH" "/usr/local/rvm/gems/ruby-1.9.1-p378")
+(setenv "MY_RUBY_HOME" "/usr/local/rvm/rubies/ruby-1.9.1-p378")
+(setenv "IRBRC" "/usr/local/rvm/rubies/ruby-1.9.1-p378/.irbrc")
+
+(provide 'rvm)
