874fbbee8c6b7bc6a0d326ecd438607ec1e0150f
Roman Scherer <roman@soundclaat.(none)>
2010-04-03 16:14:26 +0200

    Added gnus config using msmtp.

--- .gnus.el
+++ .gnus.el
@@ -1,7 +1,10 @@
 
-(setq smtp-accounts
-      '((ssl "roman.scherer@burningswell.com" "smtp.gmail.com" 587 nil nil)
-        (ssl "roman@soundcloud.com" "smtp.gmail.com" 587 nil nil)))
+(setq mail-from-style nil
+      message-send-mail-function 'message-send-mail-with-sendmail
+      message-sendmail-extra-arguments '("-a" "Soundcloud")
+      sendmail-program "/usr/bin/msmtp"
+      user-full-name "Roman Scherer"
+      user-mail-address "roman@soundcloud.com")
 
 (setq gnus-select-method '(nnfolder ""))
 
@@ -19,57 +22,17 @@
                 (nnimap-expunge-on-close always)
                 (nnimap-authinfo-file "~/.authinfo"))))
 
-(setq gnus-posting-styles
-      '(("Burningswell" (address "roman.scherer@burningswell.com"))
-        ("Soundcloud" (address "roman@soundcloud.com"))))
-
 (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
-(require 'smtpmail)
+(setq gnus-posting-styles
+      '(("Burningswell"
+         (address "roman.scherer@burningswell.com")
+         (eval (setq message-sendmail-extra-arguments '("-a" "Burningswell"))))
+        ("Soundcloud"
+         (address "roman@soundcloud.com")
+         (eval (setq message-sendmail-extra-arguments '("-a" "Soundcloud")))
+         (signature-file "~/.emacs.d/soundcloud.signature"))))
 
-(setq mail-from-style nil
-      message-send-mail-function 'smtpmail-send-it
-      send-mail-function 'smtpmail-send-it
-      smtpmail-debug-info t
-      smtpmail-debug-verb t
-      user-full-name "Roman Scherer"
-      user-mail-address "roman@soundcloud.com")
+(gnus-demon-add-handler 'gnus-group-get-new-news 2 t)
+(gnus-demon-init)
 
-(defun set-smtp-plain (address server port)
-  "Set related SMTP variables for supplied parameters."
-  (setq smtpmail-auth-credentials "~/.authinfo"
-        smtpmail-smtp-service port
-        smtpmail-starttls-credentials nil
-        smtpmail-smtp-server server
-        user-mail-address address)
-  (message "Setting SMTP server for '%s' to '%s:%s'." address server port))
-
-(defun set-smtp-ssl (address server port key cert)
-  "Set related SMTP and SSL variables for supplied parameters."
-  (setq smtpmail-auth-credentials "~/.authinfo"
-        smtpmail-smtp-server server
-        smtpmail-smtp-service port
-        smtpmail-starttls-credentials (list (list server port key cert))
-        starttls-extra-arguments nil
-        starttls-gnutls-program "gnutls-cli"
-        starttls-use-gnutls t
-        user-mail-address address)
-  (message "Setting SMTP server for '%s' to '%s:%s' (SSL)." address server port))
-
-(defun change-smtp ()
-  "Change the SMTP server according to the current from line."
-  (save-excursion
-    (loop with from = (save-restriction
-                        (message-narrow-to-headers)
-                        (message-fetch-field "from"))
-          for (acc-type address server port key cert) in smtp-accounts
-          when (string-match address from)
-          do (cond
-              ((eql acc-type 'plain)
-               (return (set-smtp-plain address server port key cert)))
-              ((eql acc-type 'ssl)
-               (return (set-smtp-ssl address server port key cert)))
-              (t (error "Unrecognized SMTP account type: '%s'." acc-type)))
-          finally (error "Cannot interfere SMTP information."))))
-
-(add-hook 'message-send-hook 'change-smtp)
