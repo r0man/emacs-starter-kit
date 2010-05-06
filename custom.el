48c4aded661a62dff8f6d5518c26f4792cbaf6fa
Roman Scherer <roman@soundcloud.com>
2010-05-06 10:27:57 +0200

    CSS indent offset is now 2.

--- custom.el
+++ custom.el
@@ -8,4 +8,11 @@
  '(comint-input-ignoredups t)
  '(comint-move-point-for-output t)
  '(comint-scroll-show-maximum-output t)
- '(comint-scroll-to-bottom-on-input t))
+ '(comint-scroll-to-bottom-on-input t)
+ '(css-indent-offset 2))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
