64a6460961f97cba3d118fcb4b378169dc1d6c4c
Roman <roman@burningswell.com>
2011-01-15 17:17:47 +0100

    Doc.

--- roman.el
+++ roman.el
@@ -155,6 +155,7 @@ So you can bind it to both M-r and M-s."
 ;; Show the menu-bar, but not the scroll-bar.
 (if (fboundp 'menu-bar-mode) (menu-bar-mode t))
 
+;; Insert newlines to avoid `end of buffer' errors.
 (setq next-line-add-newlines t)
 
 ;; Use my custom color theme.
